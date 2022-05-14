7628d10b91b8e3fecaa47d7af5beaf5d759046a6
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2022-05-15 01:48:18 +1000

    Added LargerScale Mod
    Discord request. Allows you to go as low as 1 (use 0 and the world doesn't render).  Haven't tested how high it goes, the games default is 2.

--- GYK-Mods/LargerScale/Config.cs
+++ GYK-Mods/LargerScale/Config.cs
@@ -1,22 +1,23 @@
 ﻿using System.IO;
 using System.Text;
 
-namespace UltraWide
+namespace LargerScale
 {
     public class Config
     {
         private static Options _options;
-        private const string Path = "./QMods/UltraWide/config.txt";
+        private const string Path = "./QMods/LargerScale/config.txt";
 
         public class Options
         {
             public int Width = GameSettings.me.res_x;
             public int Height = GameSettings.me.res_y;
+            public int Scale = 2;
         }
 
         private static void SaveDefaultOptions()
         {
-            var defaultOptions = new[] { _options.Width.ToString(), _options.Height.ToString() };
+            var defaultOptions = new[] { _options.Width.ToString(), _options.Height.ToString(), _options.Scale.ToString() };
             File.WriteAllLines(Path, defaultOptions, Encoding.Default);
         }
 
@@ -24,15 +25,18 @@ namespace UltraWide
         {
             if (_options != null) return _options;
             _options = new Options();
+         
             if (File.Exists(Path))
             {
                 var config = File.ReadAllLines(Path);
                 if (config.Length > 0)
                 {
-                    var isWidthNo = int.TryParse(config[0], out var tempW);
+                    var isWidthNo  = int.TryParse(config[0], out var tempW);
                     _options.Width = isWidthNo ? tempW : GameSettings.me.res_x;
-                    var isHeightNo = int.TryParse(config[1], out var tempH);
+                    var isHeightNo  = int.TryParse(config[1], out var tempH);
                     _options.Height = isHeightNo ? tempH : GameSettings.me.res_y;
+                    var isScaleNo = int.TryParse(config[2], out var tempS);
+                    _options.Scale = isScaleNo ? tempS : 2;
                 }
                 else
                 {
---
+++ GYK-Mods/LargerScale/MainPatcher.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+using Harmony;
+
+namespace LargerScale
+{
+    public class MainPatcher
+    {
+        private static Config.Options _cfg;
+
+        public static void Patch()
+        {
+            _cfg = Config.GetOptions();
+            var val = HarmonyInstance.Create($"p1xel8ted.graveyardkeeper.LargerScale");
+            val.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(ResolutionConfig), nameof(ResolutionConfig.GetResolutionConfigOrNull))]
+        public static class Patcher
+        {
+            [HarmonyPostfix]
+            static void Postfix(ref ResolutionConfig __result)
+            {
+                    var config = new ResolutionConfig(_cfg.Width, _cfg.Height)
+                {
+                    pixel_size = _cfg.Scale
+                };
+                __result = config;
+            }
+        }
+    }
+}
